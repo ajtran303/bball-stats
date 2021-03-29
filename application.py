d0cf9f6595666274642ac637201fd7b7b8ed8331
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-03-29 17:44:01 -0600

    Add clean_data function

--- application.py
+++ application.py
@@ -2,5 +2,23 @@ from constants import (TEAMS as teams,
                        PLAYERS as players)
 
 
+def clean_data(player):
+    player = player.copy()
+    player['experience'] = _convert_experience(player['experience'])
+    player['height'] = _convert_height(player['height'])
+    return player
+
+
+def _convert_height(height):
+    return int(height.partition(' ')[0])
+
+
+def _convert_experience(experience):
+    if experience == 'YES':
+        return True
+    elif experience == 'NO':
+        return False
+
+
 if __name__ == '__main__':
     pass
