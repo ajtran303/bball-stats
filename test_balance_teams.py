dd4b5ffad93df34f9ba48786cf3b096e87dcfc4c
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-03-29 22:45:02 -0600

    Use better player data in balance teams tests

--- test_balance_teams.py
+++ test_balance_teams.py
@@ -5,7 +5,32 @@ from application import balance_teams
 class TestBalanceTeams(unittest.TestCase):
     def setUp(self):
         self.teams = ['Panthers', 'Bandits', 'Warriors']
-        self.players = list(map(lambda x: f'Player {str(x)}', range(1, 19)))
+        self.players = [
+            {
+                'name': 'Herschel Krustofski',
+                'experience': True
+            },
+            {
+                'name': 'Eva Gordon',
+                'experience': False
+            },
+            {
+                'name': 'Ben Finkelstein',
+                'experience': False
+            },
+            {
+                'name': 'Joe Smith',
+                'experience': True
+            },
+            {
+                'name': 'Diego Soto',
+                'experience': True
+            },
+            {
+                'name': 'Kimmy Stein',
+                'experience': False
+            }
+        ]
         self.num_players_team = len(self.players) / len(self.teams)
 
     def test_it_does_not_change_original_data(self):
@@ -31,8 +56,12 @@ class TestBalanceTeams(unittest.TestCase):
     def test_it_puts_each_player_on_only_one_team(self):
         rosters = balance_teams(self.teams, self.players)
 
-        all_unique_1 = set(rosters['Panthers']).isdisjoint(rosters['Bandits'])
-        all_unique_2 = set(rosters['Warriors']).isdisjoint(rosters['Panthers'])
-        all_unique_3 = set(rosters['Bandits']).isdisjoint(rosters['Warriors'])
+        panthers = list(map(lambda player: player['name'], rosters['Panthers']))
+        bandits = list(map(lambda player: player['name'], rosters['Bandits']))
+        warriors = list(map(lambda player: player['name'], rosters['Warriors']))
+
+        all_unique_1 = set(panthers).isdisjoint(bandits)
+        all_unique_2 = set(warriors).isdisjoint(panthers)
+        all_unique_3 = set(bandits).isdisjoint(warriors)
 
         self.assertTrue(all_unique_1 and all_unique_2 and all_unique_3)
